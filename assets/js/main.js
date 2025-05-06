bb6cfb787039b7c4384fee2f5a50984c3759d5fe
Bhisma Chandra <bhisma.chandra.tif422@polban.ac.id>
2025-05-06 20:39:33 +0700

    Add Github Links
    forgot to put the link to the github repositories + other clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,155 +1,54 @@
-"use strict";
+// Navigation highlight on scroll
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav ul li a');
 
+window.addEventListener('scroll', () => {
+    let current = '';
 
-jQuery(document).ready(function ($) {
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
 
-    jQuery(window).load(function () {
-        jQuery(".loaded").fadeOut();
-        jQuery(".preloader").delay(1000).fadeOut("slow");
-    });
-    /*---------------------------------------------*
-     * Mobile menu
-     ---------------------------------------------*/
-    $(document).on('click', '.navbar-collapse.in', function (e) {
-        if ($(e.target).is('a')) {
-            $(this).collapse('hide');
+        if (pageYOffset >= sectionTop - 300) {
+            current = section.getAttribute('id');
         }
     });
 
-    $('ul.navbar-nav li').hover(function () {
-        $(this).children('ul').stop(true, false, true).fadeToggle(300);
-    });
-    /*---------------------------------------------*
-     * STICKY scroll
-     ---------------------------------------------*/
-
-    $("").localScroll();
-
-    /*---------------------------------------------*
-     * WOW
-     ---------------------------------------------*/
-
-    var wow = new WOW({
-        mobile: false // trigger animations on mobile devices (default is true)
-    });
-    wow.init();
-
-
-    /*---------------------------------------------*
-     * Counter 
-     ---------------------------------------------*/
-
-    $('.statistic-counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-
-    /* ---------------------------------------------------------------------
-     Carousel
-     ---------------------------------------------------------------------= */
-
-    $('.main_home_slider').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: false,
-        navText: [
-            "<i class='lnr lnr-chevron-left'></i>",
-            "<i class='lnr lnr-chevron-right'></i>"
-        ],
-        autoplayHoverPause: true
-
-    });
-
-    $('.main_team').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: true,
-        navText: [
-            "<i class='lnr lnr-chevron-left'></i>",
-            "<i class='lnr lnr-chevron-right'></i>"
-        ],
-        autoplayHoverPause: true
-
-    });
-
-    $('.main_work1_content').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: true,
-        navText: [
-            "prew",
-            "next"
-        ],
-        autoplayHoverPause: true
-
-    });
-    $('.main_work1_team').owlCarousel({
-        responsiveClass: true,
-        autoplay: false,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: false,
-        navText: [
-            "prew",
-            "next"
-        ],
-        autoplayHoverPause: true
-
-    });
-
-
-// main-menu-scroll
-
-    jQuery(window).scroll(function () {
-        var top = jQuery(document).scrollTop();
-        var height = 5;
-        //alert(batas);
-
-        if (top > height) {
-            jQuery('.navbar-fixed-top').addClass('menu-scroll');
-        } else {
-            jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
         }
     });
-
-// scroll Up
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 600) {
-            $('.scrollup').fadeIn('slow');
-        } else {
-            $('.scrollup').fadeOut('slow');
-        }
-    });
-
-    $('.scrollup').click(function () {
-        $("html, body").animate({scrollTop: 0}, 1000);
-        return false;
-    });
-
-    $('.main_mix_content').imagesLoaded(function () {
-        // images have loaded
-    });
-
-
-    //End
 });
 
+// Smooth scrolling for navigation
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
 
+        window.scrollTo({
+            top: targetSection.offsetTop - 80,
+            behavior: 'smooth'
+        });
+    });
+});
 
+// Project card expansion
+const projectCards = document.querySelectorAll('.project-card');
 
+projectCards.forEach(card => {
+    const header = card.querySelector('.project-header');
 
+    header.addEventListener('click', () => {
+        card.classList.toggle('expanded');
+    });
+});
 
-
+// Font Awesome for icons (if not already included in your project)
+const faScript = document.createElement('script');
+faScript.src = 'https://kit.fontawesome.com/your-code-here.js';
+faScript.crossOrigin = 'anonymous';
+document.head.appendChild(faScript);
